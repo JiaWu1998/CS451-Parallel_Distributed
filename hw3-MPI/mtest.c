190e302e3a2226ec23254d988d051e339abc86e6
Chameleon Cloud User <cc@mpi-instance-0.novalocal>
2019-10-31 20:14:40 +0000

    know how gather works

--- hw3-MPI/mtest.c
+++ hw3-MPI/mtest.c
@@ -6,19 +6,32 @@ int main(int argc, char** argv){
 
     //4x4
     char A[16] = {
-        "a","b","c","d",
-        "e","f","g","h",
-        "1","2","3","4",
-        "5","6","7","8"
+        'a','b','c','d',
+        'e','f','g','h',
+        '1','2','3','4',
+        '5','6','7','8'
     };
 
-    MPI_Init(argc,argv);
+    char B[16];
+
+    MPI_Init(&argc,&argv);
     MPI_Comm_size(MPI_COMM_WORLD,&world_size);
     MPI_Comm_rank(MPI_COMM_WORLD,&myid);
     
     char Buf[4];
     
-    MPI_Scatter(&A,4,MPI_CHAR,&Buf,4,MPI_CHAR,0,MPI_COMM_WORLD);
-    
+    MPI_Scatter(A,4,MPI_CHAR,Buf,4,MPI_CHAR,0,MPI_COMM_WORLD);
+    MPI_Gather(Buf, 4, MPI_CHAR, B, 4, MPI_CHAR,0,MPI_COMM_WORLD);
+    // for (i=0; i<4; ++i){
+    //     printf("%c\t",Buf[i]);
+    // }
+    // printf("\n");
+
+    if(myid == 0){
+        for (i=0; i<16; ++i){
+            printf("%c\t",B[i]);
+            // printf("\n") if i % 4 == 0;
+        }
+    }
     return 0;
 }
